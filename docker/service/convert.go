266af7efa762f8a27fb7ff76b1f8949a3dbac5a1
stevensu1977 <suwei007@gmail.com>
2018-10-22 21:52:40 +0800

    add CPURealtimePeriod,CPURealtimeRuntime

--- docker/service/convert.go
+++ docker/service/convert.go
@@ -183,17 +183,19 @@ func Convert(c *config.ServiceConfig, ctx project.Context, clientFactory compose
 	memorySwappiness := int64(c.MemSwappiness)
 
 	resources := container.Resources{
-		CgroupParent:      c.CgroupParent,
-		Memory:            int64(c.MemLimit),
-		MemoryReservation: int64(c.MemReservation),
-		MemorySwap:        int64(c.MemSwapLimit),
-		MemorySwappiness:  &memorySwappiness,
-		CPUShares:         int64(c.CPUShares),
-		CPUQuota:          int64(c.CPUQuota),
-		CpusetCpus:        c.CPUSet,
-		Ulimits:           ulimits,
-		Devices:           deviceMappings,
-		OomKillDisable:    &c.OomKillDisable,
+		CgroupParent:       c.CgroupParent,
+		Memory:             int64(c.MemLimit),
+		MemoryReservation:  int64(c.MemReservation),
+		MemorySwap:         int64(c.MemSwapLimit),
+		MemorySwappiness:   &memorySwappiness,
+		CPUShares:          int64(c.CPUShares),
+		CPUQuota:           int64(c.CPUQuota),
+		CPURealtimePeriod:  int64(c.CPURealtimePeriod), //hack add CPURealtimePeriod and CPURealtimeRuntime
+		CPURealtimeRuntime: int64(c.CPURealtimeRuntime),
+		CpusetCpus:         c.CPUSet,
+		Ulimits:            ulimits,
+		Devices:            deviceMappings,
+		OomKillDisable:     &c.OomKillDisable,
 	}
 
 	networkMode := c.NetworkMode
